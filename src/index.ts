f814df1d30918cacf4a65f8670a34fe20eea608a
KushwahaVikashJ <kushwahavicky743@gmail.com>
2024-05-31 02:27:44 +0530

    added manage-restaurant api

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,18 @@ import cors from "cors";
 import "dotenv/config";
 import mongoose from "mongoose";
 import myUserRoute from "./routes/MyUserRoute";
+import myRestaurantRoute from "./routes/MyRestaurantRoute";
+import { v2 as cloudinary } from "cloudinary";
 
 mongoose
   .connect(process.env.MONGODB_CONNECTION_STRING as string)
   .then(() => console.log("Database connected")); //db connection
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 const app = express(); //initialize a server
 app.use(express.json()); //to convert the request body of api's into json
@@ -17,5 +25,6 @@ app.get("/health", (req: Request, res: Response) => {
 });
 
 app.use("/api/my/user", myUserRoute);
+app.use("/api/my/restaurant", myRestaurantRoute);
 
 app.listen(8000, () => console.log("Server started on localhost: 8000")); //to listen the server
